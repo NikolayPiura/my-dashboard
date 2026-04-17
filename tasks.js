e9c47287f157434400e16ecdbd39c3cd4d1d055a
NikolayPiura <kol9932@gmail.com>
2026-04-17 07:49:43 -0400

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -6,15 +6,15 @@
 // ═══════════════════════════════════════════════════════
 
 var TODAY_TASKS = [
-  { time: '08:00', name: 'Коммуникации в Климате',         cat: 'work',     dur: 60  },
-  { time: '09:00', name: 'Работа по КЭ',                   cat: 'focus',    dur: 60  },
-  { time: '09:00', name: 'Работа по Фонду Друг',           cat: 'work',     dur: 60  },
-  { time: '10:00', name: 'Работа по ПФФ',                  cat: 'work',     dur: 30  },
-  { time: '10:30', name: 'Прочитать книгу (половину)',     cat: 'study',    dur: 30  },
-  { time: '11:00', name: 'Начать писать письмо',           cat: 'focus',    dur: 60  },
+  { time: '08:00', name: 'Коммуникации',                   cat: 'work',     dur: 30  },
+  { time: '08:30', name: 'Работа по КЭ',                   cat: 'focus',    dur: 30  },
+  { time: '09:00', name: 'Работа по ТЗ',                   cat: 'work',     dur: 30  },
+  { time: '09:30', name: 'Работа по ПФФ',                  cat: 'work',     dur: 30  },
+  { time: '10:00', name: 'Работа по Фонду Друг',           cat: 'work',     dur: 60  },
+  { time: '11:00', name: 'Написать главу книги',           cat: 'focus',    dur: 60  },
   { time: '12:00', name: 'Обед',                           cat: 'personal', dur: 60  },
   { time: '13:00', name: 'Время с Сашей',                  cat: 'personal', dur: 120 },
-  { time: '15:00', name: 'Доделать письмо',                cat: 'focus',    dur: 60  },
-  { time: '16:00', name: 'Учить стих',                     cat: 'study',    dur: 120 },
-  { time: '18:00', name: 'Настольный теннис',              cat: 'sport',    dur: 90  },
+  { time: '15:00', name: 'Закончить учить стих',           cat: 'study',    dur: 60  },
+  { time: '16:00', name: 'Доделать дашборд плана на день', cat: 'focus',    dur: 60  },
+  { time: '17:00', name: 'Прогресс по обучению',           cat: 'study',    dur: 60  },
 ];
