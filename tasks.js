22ccac64711a418c5d1bed440cef19e80db84804
NikolayPiura <kol9932@gmail.com>
2026-04-17 07:51:04 -0400

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -14,7 +14,9 @@ var TODAY_TASKS = [
   { time: '11:00', name: 'Написать главу книги',           cat: 'focus',    dur: 60  },
   { time: '12:00', name: 'Обед',                           cat: 'personal', dur: 60  },
   { time: '13:00', name: 'Время с Сашей',                  cat: 'personal', dur: 120 },
-  { time: '15:00', name: 'Закончить учить стих',           cat: 'study',    dur: 60  },
-  { time: '16:00', name: 'Доделать дашборд плана на день', cat: 'focus',    dur: 60  },
+  { time: '15:00', name: 'Закончить учить стих',           cat: 'study',    dur: 30  },
+  { time: '15:30', name: 'Почитать книгу',                 cat: 'study',    dur: 60  },
+  { time: '16:00', name: 'Доделать дашборд плана на день', cat: 'focus',    dur: 30  },
+  { time: '16:30', name: 'Сделать урок',                   cat: 'study',    dur: 60  },
   { time: '17:00', name: 'Прогресс по обучению',           cat: 'study',    dur: 60  },
 ];
