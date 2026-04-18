18cdcefe65e43e65dc2a994aa520fa8698dfcf1a
NikolayPiura <kol9932@gmail.com>
2026-04-18 07:54:52 -0400

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -6,17 +6,6 @@
 // ═══════════════════════════════════════════════════════
 
 var TODAY_TASKS = [
-  { time: '08:00', name: 'Коммуникации',                   cat: 'work',     dur: 30  },
-  { time: '08:30', name: 'Работа по КЭ',                   cat: 'focus',    dur: 30  },
-  { time: '09:00', name: 'Работа по ТЗ',                   cat: 'work',     dur: 30  },
-  { time: '09:30', name: 'Работа по ПФФ',                  cat: 'work',     dur: 30  },
-  { time: '10:00', name: 'Работа по Фонду Друг',           cat: 'work',     dur: 60  },
-  { time: '11:00', name: 'Написать главу книги',           cat: 'focus',    dur: 60  },
-  { time: '12:00', name: 'Обед',                           cat: 'personal', dur: 60  },
-  { time: '13:00', name: 'Время с Сашей',                  cat: 'personal', dur: 120 },
-  { time: '15:00', name: 'Закончить учить стих',           cat: 'study',    dur: 30  },
-  { time: '15:30', name: 'Почитать книгу',                 cat: 'study',    dur: 60  },
-  { time: '16:00', name: 'Доделать дашборд плана на день', cat: 'focus',    dur: 30  },
-  { time: '16:30', name: 'Сделать урок',                   cat: 'study',    dur: 60  },
-  { time: '17:00', name: 'Прогресс по обучению',           cat: 'study',    dur: 60  },
+  { time: '08:00', name: 'Созвон с Георгием',      cat: 'work',     dur: 180 },
+  { time: '11:00', name: 'Созвон с Александрой',   cat: 'work',     dur: 60  },
 ];
